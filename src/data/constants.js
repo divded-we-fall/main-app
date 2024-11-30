64fde3d64cf5013f19eff61d0020a4a459673f7a
Tony Qiu <tonyqiu@Mac-mini.local>
2024-11-30 14:50:00 -0500

    t

--- src/data/constants.js
+++ src/data/constants.js
@@ -1,6 +1,6 @@
 const gameScreenWidth = 1300;
 const blockWidth = 394;
 const gameScreenHeight = 620;
-const numberOfBlocksPerGame = 20;
+const numberOfBlocksPerGame = 30;
 
 export { gameScreenWidth, blockWidth, gameScreenHeight, numberOfBlocksPerGame };
