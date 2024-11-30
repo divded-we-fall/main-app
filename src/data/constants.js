581712450648b259b79c5e476cf6a2b1932a8cd8
Tony Qiu <tonyqiu@Mac-mini.local>
2024-11-30 14:55:07 -0500

    change constants

--- src/data/constants.js
+++ src/data/constants.js
@@ -1,6 +1,6 @@
 const gameScreenWidth = 1300;
 const blockWidth = 394;
-const gameScreenHeight = 620;
+const gameScreenHeight = 720;
 const numberOfBlocksPerGame = 30;
 
 export { gameScreenWidth, blockWidth, gameScreenHeight, numberOfBlocksPerGame };
