d011174dd912ce0c3fde28d25363254ddbc2cd63
Tejesh147 <tejesh.a147@gmail.com>
2024-11-30 14:55:21 -0500

    changed constants file

--- src/data/constants.js
+++ src/data/constants.js
@@ -1,6 +1,6 @@
 const gameScreenWidth = 1300;
 const blockWidth = 394;
 const gameScreenHeight = 720;
-const numberOfBlocksPerGame = 30;
+const numberOfBlocksPerGame = 31;
 
 export { gameScreenWidth, blockWidth, gameScreenHeight, numberOfBlocksPerGame };
