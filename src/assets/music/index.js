a90dc89b08fcf3307ef942c7b9056e65d48774d5
Tony Qiu <tonyqiu@Mac-mini.local>
2024-12-01 23:44:59 -0500

    test

---
+++ src/assets/music/index.js
@@ -0,0 +1,14 @@
+// index.js
+import DexArsonRound1 from './Dex Arson - Round 1.mp3';
+import Dimrain47AtTheSpeedOfLight from './Dimrain47 - At the Speed of Light.mp3';
+import GloriousMorning from './Glorious Morning.mp3';
+import MDKPressStart from './MDK - Press Start [FREE DOWNLOAD].mp3';
+import RoboticNightmares from './Robotic Nightmares.mp3';
+
+export {
+  DexArsonRound1,
+  Dimrain47AtTheSpeedOfLight,
+  GloriousMorning,
+  MDKPressStart,
+  RoboticNightmares,
+};
--- src/data/levelData.js
+++ src/data/levelData.js
@@ -1,51 +1,56 @@
-import { DonPollo, TalkTuah, Ronaldo, KSISong, memenome} from '../assets/videos/index'
+import {
+  DexArsonRound1,
+  Dimrain47AtTheSpeedOfLight,
+  GloriousMorning,
+  MDKPressStart,
+  RoboticNightmares,
+} from '../assets/music/index';
+import { DonPollo, TalkTuah, Ronaldo, KSISong, memenome } from '../assets/videos/index';
 
-// Level Data Configuration
 export const LevelData = {
   1: {
     name: "Peak Podcast",
     color: "to-sky-300",
     videoUrl: TalkTuah,
-    music: "src/assets/music/MDK - Press Start [FREE DOWNLOAD].mp3",
+    music: MDKPressStart,
     initialBlockWidth: 192,
     speed: 2,
-    videoLength: 7
+    videoLength: 7,
   },
   2: {
     name: "Memenome",
     color: "to-pink-300",
     videoUrl: DonPollo,
-    music: "src/assets/music/Dex Arson - Round 1.mp3",
+    music: DexArsonRound1,
     initialBlockWidth: 180,
     speed: 1.25,
-    videoLength: 15
+    videoLength: 15,
   },
   3: {
     name: "Song of the year",
     color: "to-yellow-300",
     videoUrl: KSISong,
-    music: "src/assets/music/Glorious Morning.mp3",
+    music: GloriousMorning,
     initialBlockWidth: 180,
     speed: 1,
-    videoLength: 21
+    videoLength: 21,
   },
   4: {
     name: "Unexpected GOAT",
     color: "to-green-300",
     videoUrl: Ronaldo,
-    music: "src/assets/music/Robotic Nightmares.mp3",
+    music: RoboticNightmares,
     initialBlockWidth: 180,
     speed: 0.9,
-    videoLength: 12
+    videoLength: 12,
   },
   5: {
     name: "Finale",
     color: "to-red-300",
     videoUrl: memenome,
-    music: "src/assets/music/Dimrain47 - At the Speed of Light.mp3",
+    music: Dimrain47AtTheSpeedOfLight,
     initialBlockWidth: 180,
     speed: 0.75,
-    videoLength: 38
+    videoLength: 38,
   },
-  // Add more levels...
 };
